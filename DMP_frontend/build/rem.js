424a126b39cfa6f26795eedd4eb57a882a7e1f33
lil_hu <785167327@qq.com>
2023-12-22 17:20:15 +0800

    更新分辨率适配

--- DMP_frontend/build/rem.js
+++ DMP_frontend/build/rem.js
@@ -3,7 +3,7 @@ const baseSize = 16
 // 设置 rem 函数
 function setRem () {
   // 当前页面宽度相对于  宽的缩放比例，可根据自己需要修改。
-  const scale = document.documentElement.clientWidth / 1920
+  const scale = document.documentElement.clientWidth / 1470
   // 设置页面根节点字体大小
   document.documentElement.style.fontSize = (baseSize * Math.min(scale, 2)) + 'px'
 }
